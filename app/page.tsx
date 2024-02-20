1b8b7f3887af1995377faf2e2178c89f56875175
sj <shekhar09jha@gmail.com>
2024-02-20 22:17:52 +0530

    change style

--- app/page.tsx
+++ app/page.tsx
@@ -4,35 +4,32 @@ export default function Home() {
 	return (
 		<div className="flex items-left mx-auto h-screen max-w-screen-md sm:pt-10 sm:text-left px-5 md:max-w-2">
 			<title>Shekhar Jha </title>
-			<div className="md:flex flex-col py-2 px-8">
+			<div className="md:flex flex-col py-1 px-8">
 				<Image src="/pfp.png" width={80} height={80} alt="Shekhar Jha" />
-				<div>
-					<span className="font-medium text-base">Shekhar Jha</span>
-				</div>
-				<div className="pb-4">
-					<span className="font-normal text-sm text-gray-500 tracking-wide">
+				<div className="pb-3">
+					<span className="font-normal text-xs">Shekhar Jha, </span>
+					<span className="font-normal text-xs text-gray-500">
 						Engineering at{' '}
 					</span>
 					<a
 						href="https://getminima.com"
-						className="font-normal text-sm underline underline-offset-4 decoration-1 decoration-gray-300 hover:text-blue-400 hover:decoration-blue-400"
+						className="font-normal text-xs underline underline-offset-4 decoration-1 decoration-gray-300 hover:text-blue-400 hover:decoration-blue-400"
 					>
 						Minima
 					</a>
-					.
 				</div>
 
 				<div className="text-xs">
-					<span className="text-gray-500 font-medium">My software </span>
+					<span className="text-gray-500 font-normal">My software </span>
 					<a
 						href="https://www.joelonsoftware.com/2009/09/23/the-duct-tape-programmer/"
-						className="font-medium underline underline-offset-4 decoration-1 decoration-gray-300 hover:text-blue-400 hover:decoration-blue-400"
+						className="font-normal underline underline-offset-4 decoration-1 decoration-gray-300 hover:text-blue-400 hover:decoration-blue-400"
 					>
-						philosophy
+						philosophy.
 					</a>
 				</div>
-				<div className="py-4 text-xs">
-					<p className="pb-2">
+				<div className="py-2 text-xs">
+					<p className="pb-1">
 						Email -{' '}
 						<a
 							href="mailto:shekharj@protonmail.com"
